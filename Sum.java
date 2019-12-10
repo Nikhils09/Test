98760f890b6c052fa988c4c1a900459396940eb1
Nikhils09 <sawantnikhil1998@gmail.com>
2019-12-11 03:45:35 +0530

    mm1

--- Sum.java
+++ Sum.java
@@ -2,13 +2,7 @@ import java.util.*;
 public class Sum
 {  static Scanner sc=new Scanner(System.in);
 	public static void main(String[] args) {
-	    int a,b,s=0;
-	    System.out.println("enter two no");
-	    a=sc.nextInt();
-	    b=sc.nextInt();
-	    s=a+b;
-	    System.out.print("sum is:/n "+s);
-		
+	    System.out.print("hello");	
 	}
 }
 
